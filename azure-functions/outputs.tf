27794d623c41a7663ed5b6b2ba18a12ed2d45f28
Wiktor1305 <91824446+Wiktor1305@users.noreply.github.com>
2022-05-22 10:01:33 +0200

    Update outputs.tf

--- azure-functions/outputs.tf
+++ azure-functions/outputs.tf
@@ -1,4 +1,4 @@
-resource "azurerm_function_app" "example" {
-  name            = "test-azure-functions"
-  function_app_id = azurerm_function_app.example.id
+resource "azurerm_function_app_function" "example" {
+  name                     = "test-azure-functions"
+  function_app_function_id = azurerm_function_app_function.example.id
 }
